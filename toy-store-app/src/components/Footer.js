531117bba0ec0a8dcdd2a8b449b47b601472651d
skinhead12 <edithr_isc@hotmail.com>
2023-01-19 02:24:27 -0600

    :sparkles: creating branch and upload all forms

---
+++ toy-store-app/src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+function Footer(){
+    return (
+        <>
+        <footer className="footer-container">
+            <h2>Footer</h2>
+            
+            <label>Siguenos en nuestras redes sociales</label>
+            <div className='icon-container'>
+
+            </div>
+            <label>Todos los derechos reservados</label>
+        </footer>
+        </>
+    )
+}
+
+export default Footer;
